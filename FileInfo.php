49843cafa405fa5a670d163d75eb850480daf711
Makoto Hattori <55674957+makohatto@users.noreply.github.com>
2019-11-13 06:44:19 +0900

    Create FileInfo.php
    改行コードを調べるクラス

---
+++ FileInfo.php
@@ -0,0 +1,40 @@
+<?php
+class FileInfo {
+  private $retCode;
+
+
+  //ファイルを読み込んで調べるメソッド
+  public function readFile($file){
+    $this->retCode['CRLF'] = 0; //改行コードがCRLFの行数
+    $this->retCode['LF'] = 0; //改行コードがCRLFの行数
+
+    $fp = fopen($file, 'r');
+    if(! is_resource($fp)){
+      die('ファイルを開けませんでした。');
+    }
+
+    while(($line = fgets($fp)) !==false){
+      if(preg_match('/￥r￥n￥z/', $line)){
+        $this->retCode['CRLF']++;
+      }else {
+        $this->retCode['LF']++;
+      }
+    }
+    fclose($fp);
+  }
+
+  //改行コードを返すメソッド
+  public function getRetCode(){
+    if($this->retCode['CRLF'] == 0 && $this->retCode['LF'] == 0){
+      return ''; //改行なしの場合
+    } elseif($this->retCode['CRLF'] == 0){
+      return 'LF'; //全てLFの場合
+    } elseif ($this->retCode['LF'] == 0) {
+      return 'CRLF';// 全てCRLFの場合
+    } else{
+      return 'CRLF & LF'; //CRLFとLFが混在している場合
+    }
+  }
+}
+
+ ?>
